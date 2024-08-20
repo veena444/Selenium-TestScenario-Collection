3053fcd8c3ab1052c028dc69f42161071b5cb680
Veena Hegde <veena444@gmail.com>
2024-08-20 15:09:48 +0530

    trying to push

--- src/main/java/seleniumPrograms/LaunchBrowserWithoutHttps.java
+++ src/main/java/seleniumPrograms/LaunchBrowserWithoutHttps.java
@@ -7,7 +7,7 @@ public class LaunchBrowserWithoutHttps {
 
 	public static void main(String[] args) {
 		WebDriver driver  = new ChromeDriver();
-		driver.get("www.google.com");
+		driver.get("www.google.com");//InvalidArgumentException: invalid argument
 
 	}
 

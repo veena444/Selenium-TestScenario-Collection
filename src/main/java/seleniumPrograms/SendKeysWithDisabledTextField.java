4bcabf4d1b3abbffec4a2c76dcd3f9b1d2b4da0a
Veena Hegde <veena444@gmail.com>
2024-08-29 14:44:44 +0530

    sendKeys() method with disabled text filed

---
+++ src/main/java/seleniumPrograms/SendKeysWithDisabledTextField.java
@@ -0,0 +1,26 @@
+package seleniumPrograms;
+
+/*
+ * What happens when we try to enter the value in disabled text field?
+ * 
+ * It will throw an exception; 'ElementNotInteractableException: element not interactable'
+ */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SendKeysWithDisabledTextField {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://seleniumpractise.blogspot.com/2016/09/how-to-work-with-disable-textbox-or.html");
+		
+		By password = By.id("pass");
+		
+		//try to enter the value in disabled text field
+		
+		driver.findElement(password).sendKeys("test@123");//ElementNotInteractableException
+
+	}
+
+}

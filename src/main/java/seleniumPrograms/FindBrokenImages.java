798ca54a03c54bd57bfefdfac509cddd4b412c2b
Veena Hegde <veena444@gmail.com>
2025-02-06 15:46:21 +0530

    Modified find broken images program

--- src/main/java/seleniumPrograms/FindBrokenImages.java
+++ src/main/java/seleniumPrograms/FindBrokenImages.java
@@ -14,40 +14,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FindBrokenImages {
 
 	public static void main(String[] args) {
-//		WebDriver driver = new ChromeDriver();
-//		driver.get("https://the-internet.herokuapp.com/broken_images");
-//
-//		List<WebElement> imagesList = driver.findElements(By.tagName("img"));
-//		
-//		for(WebElement e:imagesList ) {
-//			String url = e.getDomAttribute("src");
-//			verifyBrokenImages(url);
-//		}
-//		
-//		driver.quit();
-//		
-//	}
-//	
-//	public static void verifyBrokenImages(String linkUrl) {
-//		try {
-//
-//		    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(linkUrl).openConnection();
-//		    httpURLConnection.setConnectTimeout(5000);
-//		    httpURLConnection.connect();
-//		    
-//		    if(httpURLConnection.getResponseCode() != 200) {
-//				System.out.println(linkUrl +" ---> " + httpURLConnection.getResponseMessage() + " is a broken image");
-//			}
-//		    else {
-//		    	System.out.println(linkUrl +" ---> " + httpURLConnection.getResponseMessage());
-//		    }
-//		    
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//		}
-//		catch (IOException e) {
-//			e.printStackTrace();
-//		}		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/broken_images");
+
+		List<WebElement> imagesList = driver.findElements(By.tagName("img"));
+		
+		String baseUrl = "https://the-internet.herokuapp.com";
+		
+		
+		for(WebElement e:imagesList ) {
+			String imageUrl = e.getDomAttribute("src");
+			
+			if(imageUrl != null && imageUrl.isEmpty()) {
+                if (!imageUrl.startsWith("http")) {
+                    imageUrl = baseUrl + imageUrl;
+                }
+			
+			verifyBrokenImages(imageUrl);
+		}
+		else {
+			System.out.println("Skipping empty or null image URL.");
+		}
+	}		
+		
+		driver.quit();
+		
+	}
+	
+	public static void verifyBrokenImages(String linkUrl) {
+		try {
+
+		    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(linkUrl).openConnection();
+		    httpURLConnection.setConnectTimeout(5000);
+		    httpURLConnection.connect();
+		    
+		    if(httpURLConnection.getResponseCode() != 200) {
+				System.out.println(linkUrl +" ---> " + httpURLConnection.getResponseMessage() + " is a broken image");
+			}
+		    else {
+		    	System.out.println(linkUrl +" ---> " + httpURLConnection.getResponseMessage());
+		    }
+		    
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}		
 		
 	}
 
